9a71a0bc9fcf681a6eaa91ac3e58ebe145e82c26
Matthew Caraway <caraway602@hotmail.com>
2024-07-03 21:45:44 -0600

    Adding New Relic

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import newrelic.agent
+import os
+
+# Load environment variables from the .env file
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+# Initialize New Relic agent
+newrelic.agent.initialize('newrelic.ini')
+
 import httpx
 import asyncio
 from fastapi import FastAPI, HTTPException, BackgroundTasks
@@ -8,14 +18,9 @@ from io import BytesIO
 from pdfminer.high_level import extract_text
 from ResumeProcessor.JobRecomendation import main as job_recommendation_main
 
-import os
 import json
 import time
-from dotenv import load_dotenv
-import requests
 import pandas as pd
-from pdfminer.high_level import extract_text
-
 from ResumeProcessor.LlmResumeProcessor import LlmResumeProcessor
 from ResumeProcessor.CustomReportGenerator import main as custom_report_generator_main
 from ResumeProcessor.RecommendedJobsEmailer import main as recomended_jobs_reporter_main
@@ -23,10 +28,8 @@ from ResumeProcessor.RecommendedJobsEmailer import main as recomended_jobs_repor
 numberOfJobsForRecomendation = 1000
 numberOfJobsForReportGeneration = 5
 
-load_dotenv('.env')
 log_level = os.getenv('LOG_LEVEL', 'INFO')
 logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 app = FastAPI()
 
@@ -35,7 +38,6 @@ class RecommendationRequest(BaseModel):
     email_address: str
     resume_url: str
 
-
 async def download_pdf(resume_url: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(resume_url)
@@ -43,43 +45,25 @@ async def download_pdf(resume_url: str):
         return BytesIO(response.content)
 
 async def jobRecommendation(email_address: str, resume_url: str):
-
     try:
-
-        # Begin the timer
         start_time = time.time()
-
         logging.debug(f"Begin Job Recomendation for email: {email_address}.")
         logging.debug(f"Resume url is located at: {resume_url}.")
-
         pdfFile = await download_pdf(resume_url)
         resumeAsString = extract_text(pdfFile)
-
         llmResumeProcessor = LlmResumeProcessor()
         resumeAsJson = llmResumeProcessor.parseResume(resumeAsString)
         resumeAsJson = json.loads(resumeAsJson)
-
         candidateName = resumeAsJson["Name"]
-
         recommendedJobsDf = job_recommendation_main(resumeAsJson, numberOfJobsForRecomendation, resumeAsString)
         logging.debug(f"Completed the job recomendation step: {resume_url}.")
-
         topRecomendedJobs = recommendedJobsDf["job_posting_id"].head(numberOfJobsForReportGeneration).tolist()
-
         recomendedJobsAsJobIdList = custom_report_generator_main(candidateName, resumeAsString, topRecomendedJobs)
-
-        #TODO: Persist Job Recomendation to DB
-
-        #Email Job Recomendation
         recomended_jobs_reporter_main(email_address, recommendedJobsDf, recomendedJobsAsJobIdList, candidateName)
-        
-        # Stop the timer and calculate total time taken.
         finish_time = time.time()
         total_time_in_seconds = finish_time - start_time
         logging.debug(f"Finished email send. Recomendation Process has completed")
-        logging.INFO(f"Recomendation has complete. Time required: {total_time_in_seconds}")
-
-
+        logging.info(f"Recomendation has complete. Time required: {total_time_in_seconds}")
     except requests.RequestException as e:
         print(f"Error downloading the PDF: {e}")
     except Exception as e:
@@ -87,11 +71,5 @@ async def jobRecommendation(email_address: str, resume_url: str):
 
 @app.post("/recommendation")
 async def recommendation_endpoint(request: RecommendationRequest, background_tasks: BackgroundTasks):
-    """
-    Endpoint to receive email address and resume URL, and start a background job for processing.
-    """
-    # Kick off the background task without waiting for it to complete
     background_tasks.add_task(jobRecommendation, request.email_address, request.resume_url)
-    
-    # Return a response immediately to the client
     return {"message": "Recommendation process started successfully"}
